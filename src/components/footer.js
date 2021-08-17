a42e5919ac5480dd0f419a1352f87a0d094626a8
Hugonin <jeanhugues.doumbe@gmail.com>
2021-08-17 23:20:37 +0200

    Build the footer component and updated the layout.module.css

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
+import { Router, Link } from "@reach/router";
+import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
 
 import {
   siteFooter,
   footerMain,
-  logo,
+  footerLogo,
   footerFineprint,
   socialList,
+  socialListItem,
+  socialListLink,
 } from "./layout.module.css";
 
 const Footer = () => {
@@ -17,7 +21,7 @@ const Footer = () => {
           <StaticImage
             src="../images/logo_transparent.png"
             alt="logo"
-            className={logo}
+            className={footerLogo}
           />
           <p className={footerFineprint}>
             © {new Date().getFullYear()} Hugonin
@@ -26,8 +30,28 @@ const Footer = () => {
           </p>
         </div>
         <ul className={socialList}>
-       
+          <li className={socialListItem}>
+            <Link to="https://linkedin.com" className={socialListLink}>
+              <FaLinkedin />
+            </Link>
+          </li>
+          <li className={socialListItem}>
+            <Link to="https://twitter.com" className={socialListLink}>
+              <FaTwitter />
+            </Link>
+          </li>
+          <li className={socialListItem}>
+            <Link to="https://github.com" className={socialListLink}>
+              <FaGithub />
+            </Link>
+          </li>
         </ul>
+
+        <Router>
+          <FaLinkedin path="https://linkedin.com" />
+          <FaTwitter path="https://twitter.com" />
+          <FaGithub path="https://github.com" />
+        </Router>
       </footer>
     </div>
   );
